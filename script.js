45ac689579024f3cc858e9837cb2006db199dee4
Nazmun Nahar Khanom <65033964+TITHI-KHAN@users.noreply.github.com>
2024-01-25 17:15:35 +0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const navToggle = document.querySelector('.toggle-button');
-    const nav = document.querySelector('nav');
-
-    navToggle.addEventListener('click', function() {
-        nav.classList.toggle('nav-open');
-        nav.classList.toggle('nav-closed');
-    });
-});
